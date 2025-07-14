d4c9c6e063c660e1fdca1978935dd1cf751ac36f
CODEREaDer24 <deependpools@ymail.com>
2025-07-14 06:57:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,7 @@ def clipboard():
     return render_template('clipboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+    
